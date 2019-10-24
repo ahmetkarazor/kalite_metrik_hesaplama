9cfdd95690050b16b27fe012b717a4318d9a667b
ahmetkarazor <ahmetkarazor@users.noreply.github.com>
2019-10-24 18:39:49 +0300

    metrikhesaplama

---
+++ untitled.m
@@ -0,0 +1,186 @@
+function varargout = untitled(varargin)
+
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+
+
+function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+
+handles.output = hObject;
+guidata(hObject, handles);
+
+function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in deger1.
+
+function deger1_Callback(hObject, eventdata, handles)
+
+[filename,filepath]=uigetfile({'*.*'},'ANALÝZ EDÝLECEK RESMÝ SEÇÝNÝZ');
+selectedImage=imread(strcat(filepath,filename));
+axes(handles.axes1);
+imshow(selectedImage);
+
+A = imnoise(selectedImage,'salt & pepper', 0.02);
+axes(handles.axes3);
+imshow(A);
+
+%%%%%%%%%%%%
+err = immse(A, selectedImage);
+set(handles.text2,'String',err);
+
+%%%%%%%%%%%%
+[peaksnr, snr] = psnr(A, selectedImage);
+set(handles.text3,'String',peaksnr);
+set(handles.text4,'String',snr);
+
+%%%%%%%%%%%%
+H = fspecial('Gaussian',[11 11],1.5);
+B = imfilter(selectedImage,H,'replicate');
+[ssimval] = ssim(B,selectedImage);
+set(handles.text5,'String',ssimval);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+brisqueInoise = brisque(A);
+set(handles.text13,'String',brisqueInoise);
+%%%%%%%%%%%
+brisqueInoise1 = brisque(selectedImage);
+set(handles.text6,'String',brisqueInoise1);
+
+%%%%%%%%%%%%%%%%%%%%%%
+niqeInoise = niqe(A);
+set(handles.text14,'String',niqeInoise);
+%%%%%%%%%%%%%%%%%%%%%%
+niqeInoise1 = niqe(selectedImage);
+set(handles.text8,'String',niqeInoise1);
+
+function edit2_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit3_Callback(hObject, eventdata, handles)
+
+function edit3_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit4_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit5_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit5_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit6_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit6_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit7_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit7_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit8_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit8_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit9_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit9_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit10_Callback(hObject, eventdata, handles)
+
+function edit10_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit12_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit12_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit13_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit13_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit14_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit14_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function edit15_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function edit15_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
